6909a3ff97d5e1c9b9985d38c305c03c4819d6e4
Noa.O <noa.obringer@gmail.com>
2025-05-09 23:03:13 +0200

    🎨 style: improve header style

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,11 +6,11 @@ const GooeyNav = dynamic(() => import("./ui/GooeyNav"), { ssr: false });
 
 export default function Header() {
   return (
-    <header className="fixed top-0 left-0 right-0 z-50 backdrop-blur">
-      <div className="container mx-auto px-4">
+    <header className="fixed top-5 left-0 right-0 z-50">
+      <div className="container mx-auto px-4 backdrop-blur-sm rounded-full p-2">
         <div className="flex items-center justify-between h-16">
-          <Link href="/" className="text-white text-xl font-bold">
-            BKM Poker
+          <Link href="/" className="text-white text-xl font-bold opacity-80">
+            BKM
           </Link>
 
           <div className="absolute left-1/2 transform -translate-x-1/2">
