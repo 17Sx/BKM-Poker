4f83f9d4b4ea0d4a3eee3da38759a20035dd70a8
Noa.O <noa.obringer@gmail.com>
2025-05-12 11:22:02 +0200

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -21,7 +21,7 @@ export default function Header() {
               },
               {
                 label: "Dashboard",
-                href: "/dashboard",
+                href: "/",
               },
               {
                 label: "History",
